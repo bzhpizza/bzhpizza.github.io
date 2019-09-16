cb89f9d5bc7a7ec30269639c15df09d8bb806be4
Sylvan LE DEUNFF <sledeunf@gmail.com>
2019-09-16 18:15:02 +0200

    add dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
+import * as $ from "jquery";
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
+import { ModalModule } from "ngx-bootstrap/modal";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
@@ -9,8 +11,8 @@ import { ScheduleComponent } from "./components/schedule/schedule.component";
 import { NavigationComponent } from "./components/navigation/navigation.component";
 import { CartPopupComponent } from "./components/cart-popup/cart-popup.component";
 import { OrderComponent } from "./components/order/order.component";
-import { ConfirmationPopupComponent } from './components/confirmation-popup/confirmation-popup.component';
-import { TrustPipe } from './pipe/trust.pipe';
+import { ConfirmationPopupComponent } from "./components/confirmation-popup/confirmation-popup.component";
+import { TrustPipe } from "./pipe/trust.pipe";
 
 @NgModule({
   declarations: [
@@ -24,7 +26,7 @@ import { TrustPipe } from './pipe/trust.pipe';
     ConfirmationPopupComponent,
     TrustPipe
   ],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, ModalModule.forRoot()],
   providers: [],
   bootstrap: [AppComponent]
 })
