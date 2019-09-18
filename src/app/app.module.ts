fbebf8989b454430cce12aebf17087d0cb873512
Sylvan LE DEUNFF <sledeunf@gmail.com>
2019-09-18 12:30:44 +0200

    remove useless routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,6 @@ import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { ModalModule } from "ngx-bootstrap/modal";
 
-import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { PizzasComponent } from "./components/pizzas/pizzas.component";
 import { PizzaComponent } from "./components/pizza/pizza.component";
@@ -28,7 +27,7 @@ import { TrustPipe } from "./pipe/trust.pipe";
     ConfirmationPopupComponent,
     TrustPipe
   ],
-  imports: [BrowserModule, AppRoutingModule, ModalModule.forRoot()],
+  imports: [BrowserModule, ModalModule.forRoot()],
   providers: [],
   bootstrap: [AppComponent]
 })
